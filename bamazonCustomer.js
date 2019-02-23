57508fb22a729fcfc1e90fe1f9199e0f79301229
Jason Schutz <jason@rogueathletic.com>
2019-02-23 13:34:47 -0800

    updated and added notes to bamazinCustomers.js only read me mark down left

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,10 @@
-const mysql = require('mysql');
+/* <----- NODE.js     J.SCHUTZ */
+/* <----- NODE.js packets We are requiring into the app J.SCHUTZ */
+const mysql = require('mysql'); 
 const inquirer = require('inquirer');
 require('console.table');
 var keys = require("./keys");
-// <------ connection is the log in info for the app
+/* <----- NODE.js   all the nessisary log in info to log this program into MYSQL  J.SCHUTZ */
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,12 +12,12 @@ var connection = mysql.createConnection({
     password: "hMw8EgmunkRDyTFMMb7]f96Nkei6Fsx",
     database: "bamazon"
 });
-// <------ how your are notified if there was an error
+/* <----- NODE.js  initial error notification script. this is global, there are others that are function specific   J.SCHUTZ */
 connection.connect(function (err) {
     if (err) throw err;
     availableProducts();
 });
-// <------ how the invintory comma seperated variable list is brought into the app
+/* <----- NODE.js   this is the script that brings in the comma seperated value list from another source outside of this specific file  J.SCHUTZ */
 function availableProducts() {
     console.log("\nBike Shop Merchandise: \n");
     connection.query("SELECT id, productName, price FROM products", function (err, results) {
@@ -24,7 +26,7 @@ function availableProducts() {
         startShopping();
     });
 }
-// <------ the initial interactions between the consumer and the database. the questions responses will yeild a unique response for different info added
+/* <------ the initial interactions between the consumer and the database. the questions responses will yeild a unique response for different info added */
 function startShopping() {
     inquirer.prompt([{
             name: "itemID",
@@ -53,7 +55,6 @@ function startShopping() {
                         if (err) throw err;
                         console.log(`Your total is: ${purchaseTotal}`);
                         continueShopping();
-                        // return results; // <--- i dont think this is working or doing anything
                     });
             } else {
                 console.log("Unfortunatly at this time we are out of stock for the item you requested. Please check back later or contact our sales department to find out when the item will be back in stock.");
@@ -62,7 +63,7 @@ function startShopping() {
         });
     });
 }
-
+/* <----- NODE.js   As you have moved through the stores function you are returned the option to continue shopping or exit the app/store  J.SCHUTZ */
 function continueShopping() {
     inquirer.prompt([{
         name: "tryAgain",
@@ -80,3 +81,4 @@ function continueShopping() {
         }
     });
 }
+/* -----> ***** Many of the console.log files were formatted using ES6. This process used console.log just as it is in es5 but once within the parenthesies you will use backtick to contain all contents in the parenthesies where once you have completed your message copy you then use = and then you use $ with no space between the two symbols. Then you will want to use the curly brackets again with no symbols or spaces after the dollar sign. Inside the curly brackets is where the contents that need to be called should be placed. Once this info has been added you will close the console log with a ; at thee end as you normally would. Then move to the next line and continue your code.  ***** <----- J.SCHUTZ */
